0749a7dfd2e9960cef01bb158941237d94a0cb83
The Reaper <30888091+ReaperZ0v@users.noreply.github.com>
2021-09-28 21:31:18 +0500

    photograph

--- src/competitions.py
+++ src/competitions.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import pandas as pd 
 import requests
 import json 
+import os 
 
 
 class SiegeCompetition:
@@ -19,6 +21,34 @@ class SiegeCompetition:
         return pd.Series([
             team.text.strip() for team in self.soup_object.find_all("div", class_="col-6 col-md-4 col-lg-3 px-2 mb-3 col--has-trunk overflow-visible")
         ])
+
+    def map_picks(self, export='dataframe'):
+        map_frame = {
+            "map": [map.text.strip() for map in self.soup_object.find_all("h4", class_="map__name my-0 mr-4")],
+            "rounds": [round.text.strip() for round in self.soup_object.find_all("strong", class_="ml-auto text-center")]
+        }
+
+        if export == 'dataframe':
+            return pd.DataFrame(map_frame)
+
+        elif export == "csv":
+            if os.path.exists("exports"):
+                self.fetch_log()[0].to_csv(f"exports/map_export_{datetime.now()}.csv")
+                return True
+
+            else:
+                os.mkdir("exports")
+                self.fetch_log()[0].to_csv(f"exports/map_export_{datetime.now()}.csv")
+                return True 
+
+        elif export == 'json':
+            return json.dumps(pd.DataFrame(map_frame), indent=4)
+
+
+sgcomp = SiegeCompetition("318-polish-masters-2021")
+sgcomp.map_picks()
+
+
 
     
 
